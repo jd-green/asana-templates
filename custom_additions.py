3a1bebb1c8c59e666ef0b2f48fe3063d58d8674d
James Green <james@virtadmin.com>
2019-12-17 10:22:43 -0600

    Added Whitepaper project type

--- custom_additions.py
+++ custom_additions.py
@@ -2,31 +2,41 @@ import blog_article
 import build_task
 import tech_brief
 import webinar_wrapup
+import whitepaper
 
 client_name = input("Client Name: ")
 proj_id = input("Project ID: ")
 gid = input("Asana Project GID: ")
 
-# # Create 1 Blog Articles
-# for num in range(1):
-#     id = blog_article.create_article(client_name, str(num+1), "1")
-#     print("Blog Article Created!")
-#     print("Asana GID = "+id)
-#     url = "https://app.asana.com/0/"+id
-#     build_task.build_article_task(
-#         gid, client_name, str(num+1), url, proj_id, id)
-#     print("Article Tracking Task Created!")
-
-
-# # Create 1 Tech Brief Projects
-for num in range(1):
-    id = tech_brief.create_tech_brief(client_name, str(num+1), "1")
-    print("Tech Brief Created!")
+# Create 5 Blog Articles
+for num in range(5):
+    id = blog_article.create_article(client_name, str(num+1), "5")
+    print("Blog Article Created!")
     print("Asana GID = "+id)
     url = "https://app.asana.com/0/"+id
-    build_task.build_tb_task(
+    build_task.build_article_task(
         gid, client_name, str(num+1), url, proj_id, id)
-    print("Tech Brief Tracking Task Created!")
+    print("Article Tracking Task Created!")
+
+# # Create 3 Whitepaper Projects
+for num in range(3):
+    id = whitepaper.create_whitepaper(client_name, str(num+1), "3")
+    print("Whitepaper Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_wp_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("Whitepaper Tracking Task Created!")
+
+# # Create 1 Tech Brief Projects
+# for num in range(1):
+#     id = tech_brief.create_tech_brief(client_name, str(num+1), "1")
+#     print("Tech Brief Created!")
+#     print("Asana GID = "+id)
+#     url = "https://app.asana.com/0/"+id
+#     build_task.build_tb_task(
+#         gid, client_name, str(num+1), url, proj_id, id)
+#     print("Tech Brief Tracking Task Created!")
 
 # Create 1 Webinar Wrap-Up Paper Projects
 # for num in range(1):
