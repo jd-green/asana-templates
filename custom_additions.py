2727b6bc07341c6ce64bcb5f51587b1893ab6339
James Green <james@virtadmin.com>
2020-01-21 15:11:11 -0600

    Added features for creating full GGE

--- custom_additions.py
+++ custom_additions.py
@@ -3,34 +3,35 @@ import build_task
 import tech_brief
 import webinar_wrapup
 import whitepaper
+import gorilla_guide_express
 
 client_name = input("Client Name: ")
 proj_id = input("Project ID: ")
 gid = input("Asana Project GID: ")
 
-# # Create 3 Whitepaper Projects
-for num in range(3):
-    id = whitepaper.create_whitepaper(client_name, str(num+1), "3")
-    print("Whitepaper Created!")
-    print("Asana GID = "+id)
-    url = "https://app.asana.com/0/"+id
-    build_task.build_wp_task(
-        gid, client_name, str(num+1), url, proj_id, id)
-    print("Whitepaper Tracking Task Created!")
+# # # Create 3 Whitepaper Projects
+# for num in range(3):
+#     id = whitepaper.create_whitepaper(client_name, str(num+1), "3")
+#     print("Whitepaper Created!")
+#     print("Asana GID = "+id)
+#     url = "https://app.asana.com/0/"+id
+#     build_task.build_wp_task(
+#         gid, client_name, str(num+1), url, proj_id, id)
+#     print("Whitepaper Tracking Task Created!")
+#
+# # Create 2 Blog Articles
+# for num in range(2):
+#     id = blog_article.create_article(client_name, str(num+1), "2")
+#     print("Blog Article Created!")
+#     print("Asana GID = "+id)
+#     url = "https://app.asana.com/0/"+id
+#     build_task.build_article_task(
+#         gid, client_name, str(num+1), url, proj_id, id)
+#     print("Article Tracking Task Created!")
 
-# Create 5 Blog Articles
-for num in range(5):
-    id = blog_article.create_article(client_name, str(num+1), "5")
-    print("Blog Article Created!")
-    print("Asana GID = "+id)
-    url = "https://app.asana.com/0/"+id
-    build_task.build_article_task(
-        gid, client_name, str(num+1), url, proj_id, id)
-    print("Article Tracking Task Created!")
-
-# # Create 1 Tech Brief Projects
-# for num in range(1):
-#     id = tech_brief.create_tech_brief(client_name, str(num+1), "1")
+# # Create 4 Tech Brief Projects
+# for num in range(4):
+#     id = tech_brief.create_tech_brief(client_name, str(num+1), "4")
 #     print("Tech Brief Created!")
 #     print("Asana GID = "+id)
 #     url = "https://app.asana.com/0/"+id
@@ -47,3 +48,13 @@ for num in range(5):
 #     build_task.build_wrapup_task(
 #         gid, client_name, str(num+1), url, proj_id, id)
 #     print("Webinar Wrap-Up Paper Tracking Task Created!")
+
+# Create 3 Gorilla Guide Express Projects
+for num in range(3):
+    id = gorilla_guide_express.create_gge(client_name, str(num+1), "3")
+    print("Webinar Wrap-Up Paper Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_full_gge_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("GGE Tracking Task Created!")
