baae84cb750a4f131d39d4c0dc544f8be3660672
James Green <james@virtadmin.com>
2020-02-27 09:59:20 -0600

    Added full Gorilla Guide build functionality

--- build_task.py
+++ build_task.py
@@ -426,3 +426,55 @@ def build_full_gge_task(project, client, number, link, pid, tid):
         #     content=response.content))
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
+
+def build_full_gg_task(project, client, number, link, pid, tid):
+    data1 = """
+    {
+      "data": {
+        "name": "[tmp] Gorilla Guide",
+        "resource_subtype": "milestone",
+        "assignee": null,
+        "completed": false,
+        "followers": [
+          "50542848393516",
+          "597316423178909",
+          "1110471739427709"
+        ],
+        "notes": "theLink",
+        "parent": null,
+        "projects": [
+          "theProject",
+          "taskID"
+        ],
+        "custom_fields":
+            {
+            "1144501884560660": "1144501884560665",
+            "860330517401733": "10000",
+            "860297462878516": "5000",
+            "1145453081034849": "thePID",
+            "1145458999310530": "1145465565580000"
+            },
+        "workspace": "21756957370747"
+      }
+    }"""
+    data2 = data1.replace('theProject', project)
+    data3 = data2.replace('thePID', pid)
+    data4 = data3.replace('theNum', number)
+    data5 = data4.replace('taskID', tid)
+    theData = data5.replace('theLink', link)
+    try:
+        response = requests.post(
+            url="https://app.asana.com/api/1.0/tasks/",
+            headers={
+                "Authorization": config.api_key,
+                "Content-Type": "text/plain; charset=utf-8",
+                "Cookie": "TooBusyRedirectCount=0",
+            },
+            data=theData.replace("tmp", client)
+            )
+        # print('Response HTTP Status Code: {status_code}'.format(
+        #     status_code=response.status_code))
+        # print('Response HTTP Response Body: {content}'.format(
+        #     content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
--- custom_additions.py
+++ custom_additions.py
@@ -4,40 +4,47 @@ import tech_brief
 import webinar_wrapup
 import whitepaper
 import gorilla_guide_express
+import gg_video
 
 client_name = input("Client Name: ")
 proj_id = input("Project ID: ")
 gid = input("Asana Project GID: ")
 
-# # # Create 3 Whitepaper Projects
-# for num in range(3):
-#     id = whitepaper.create_whitepaper(client_name, str(num+1), "3")
-#     print("Whitepaper Created!")
-#     print("Asana GID = "+id)
-#     url = "https://app.asana.com/0/"+id
-#     build_task.build_wp_task(
-#         gid, client_name, str(num+1), url, proj_id, id)
-#     print("Whitepaper Tracking Task Created!")
-#
-# # Create 2 Blog Articles
-# for num in range(2):
-#     id = blog_article.create_article(client_name, str(num+1), "2")
-#     print("Blog Article Created!")
-#     print("Asana GID = "+id)
-#     url = "https://app.asana.com/0/"+id
-#     build_task.build_article_task(
-#         gid, client_name, str(num+1), url, proj_id, id)
-#     print("Article Tracking Task Created!")
+# Create 9 Blog Articles
+for num in range(9):
+    id = blog_article.create_article(client_name, str(num+1), "9")
+    print("Blog Article Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_article_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("Article Tracking Task Created!")
 
-# # Create 4 Tech Brief Projects
-# for num in range(4):
-#     id = tech_brief.create_tech_brief(client_name, str(num+1), "4")
-#     print("Tech Brief Created!")
-#     print("Asana GID = "+id)
-#     url = "https://app.asana.com/0/"+id
-#     build_task.build_tb_task(
-#         gid, client_name, str(num+1), url, proj_id, id)
-#     print("Tech Brief Tracking Task Created!")
+# Create 6 Videos
+for num in range(6):
+    id = gg_video.create_gg_video(client_name, str(num+1), "6")
+    print("Video Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_gg_video_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("Video Tracking Task Created!")
+
+# Create 3 Tech Brief Projects
+for num in range(3):
+    id = tech_brief.create_tech_brief(client_name, str(num+1), "3")
+    print("Tech Brief Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_tb_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("Tech Brief Tracking Task Created!")
+
+# Create 1 Gorilla Guide Express tracker tasks
+for num in range(1):
+    build_task.build_gge_task(gid, client_name, str(num+1),
+                              proj_id)
+    print("GGE Tracking Task Created!")
 
 # Create 1 Webinar Wrap-Up Paper Projects
 # for num in range(1):
@@ -49,12 +56,23 @@ gid = input("Asana Project GID: ")
 #         gid, client_name, str(num+1), url, proj_id, id)
 #     print("Webinar Wrap-Up Paper Tracking Task Created!")
 
-# Create 3 Gorilla Guide Express Projects
-for num in range(3):
-    id = gorilla_guide_express.create_gge(client_name, str(num+1), "3")
-    print("Gorilla Guide Express Created!")
-    print("Asana GID = "+id)
-    url = "https://app.asana.com/0/"+id
-    build_task.build_full_gge_task(
-        gid, client_name, str(num+1), url, proj_id, id)
-    print("GGE Tracking Task Created!")
+# # Create 3 Gorilla Guide Express Projects
+# for num in range(3):
+#     id = gorilla_guide_express.create_gge(client_name, str(num+1), "3")
+#     print("Gorilla Guide Express Created!")
+#     print("Asana GID = "+id)
+#     url = "https://app.asana.com/0/"+id
+#     build_task.build_full_gge_task(
+#         gid, client_name, str(num+1), url, proj_id, id)
+#     print("GGE Tracking Task Created!")
+
+# # # Create 3 Whitepaper Projects
+# for num in range(3):
+#     id = whitepaper.create_whitepaper(client_name, str(num+1), "3")
+#     print("Whitepaper Created!")
+#     print("Asana GID = "+id)
+#     url = "https://app.asana.com/0/"+id
+#     build_task.build_wp_task(
+#         gid, client_name, str(num+1), url, proj_id, id)
+#     print("Whitepaper Tracking Task Created!")
+#
