9dde5c6f9e915322bda50c16ad4ee86ea4833e31
James Green <james@virtadmin.com>
2020-04-07 09:03:38 -0500

    Added Gorilla Guide Tech Brief product

--- custom_additions.py
+++ custom_additions.py
@@ -6,23 +6,40 @@ import whitepaper
 import gorilla_guide_express
 import gg_video
 import gorilla_guide
+import gg_tech_brief
 
 client_name = input("Client Name: ")
 proj_id = input("Project ID: ")
 gid = input("Asana Project GID: ")
 
-# Create a Gorilla Guide Project
-id = gorilla_guide.create_gg(client_name)
-print("Gorilla Guide Created!")
-print("Asana GID = "+id)
-url = "https://app.asana.com/0/"+id
-build_task.build_full_gg_task(
-    gid, client_name, url, proj_id, id)
-print("GG Tracking Task Created!")
+# Create 3 Gorilla Guide Tech Brief Projects
+for num in range(3):
+    id = gg_tech_brief.create_gg_tech_brief(client_name, str(num+1), "3")
+    print("Gorilla Guide Tech Brief Created!")
+    print("Asana GID = "+id)
+    url = "https://app.asana.com/0/"+id
+    build_task.build_gg_tb_task(
+        gid, client_name, str(num+1), url, proj_id, id)
+    print("Tech Brief Tracking Task Created!")
 
-# # Create 9 Blog Articles
-# for num in range(9):
-#     id = blog_article.create_article(client_name, str(num+1), "9")
+# Create 1 Gorilla Guide Express rollup tracker tasks
+for num in range(1):
+    build_task.build_gge_task(gid, client_name, str(num+1),
+                                  proj_id)
+    print("GGE Tracking Task Created!")
+
+# # Create a Gorilla Guide Project
+# id = gorilla_guide.create_gg(client_name)
+# print("Gorilla Guide Created!")
+# print("Asana GID = "+id)
+# url = "https://app.asana.com/0/"+id
+# build_task.build_full_gg_task(
+#     gid, client_name, url, proj_id, id)
+# print("GG Tracking Task Created!")
+
+# Create 3 Blog Articles
+# for num in range(3):
+#     id = blog_article.create_article(client_name, str(num+1), "3")
 #     print("Blog Article Created!")
 #     print("Asana GID = "+id)
 #     url = "https://app.asana.com/0/"+id
@@ -30,19 +47,19 @@ print("GG Tracking Task Created!")
 #         gid, client_name, str(num+1), url, proj_id, id)
 #     print("Article Tracking Task Created!")
 #
-# # Create 6 Videos
-# for num in range(6):
-#     id = gg_video.create_gg_video(client_name, str(num+1), "6")
+# # Create 2 Videos
+# for num in range(2):
+#     id = gg_video.create_gg_video(client_name, str(num+1), "2")
 #     print("Video Created!")
 #     print("Asana GID = "+id)
 #     url = "https://app.asana.com/0/"+id
 #     build_task.build_gg_video_task(
 #         gid, client_name, str(num+1), url, proj_id, id)
 #     print("Video Tracking Task Created!")
-#
-# # Create 3 Tech Brief Projects
-# for num in range(3):
-#     id = tech_brief.create_tech_brief(client_name, str(num+1), "3")
+# #
+# # Create 1 Tech Brief Projects
+# for num in range(1):
+#     id = tech_brief.create_tech_brief(client_name, str(num+1), "1")
 #     print("Tech Brief Created!")
 #     print("Asana GID = "+id)
 #     url = "https://app.asana.com/0/"+id
